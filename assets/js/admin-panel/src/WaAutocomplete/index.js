9bd23f54045ddd9095854be546794969d52d4308
Manin Maxim <manin.maxim@mail.ru>
2024-05-30 10:54:28 +0300

    Расширяет компонент WaAutocomplete опциональными вариантами выбора
    Опциональные варианты позволяют пользователю не только выбирать записи
    из представленных вариантов выбора при инициализации, но и вводить
    собственные, которые по нажатию Enter добавляются в исходный список и
    при сбросе их выбора удаляются от туда, что делает по сути из элемента
    управления многофункциональное поле ввода, подсказывающее варианты
    выбора, а также запоминающее введенные пользователем варианты выбора в
    общий список.

--- assets/js/admin-panel/src/WaAutocomplete/index.js
+++ assets/js/admin-panel/src/WaAutocomplete/index.js
@@ -13,9 +13,10 @@ import { generateId, getTextWidth } from "../services";
 } */
 
 export default function WaAutocomplete({
-    data,
+    data: initialData,
     pholder,
     multiple = false,
+    allowNotPresent = false,
     className,
     style,
     minWidth,
@@ -27,10 +28,15 @@ export default function WaAutocomplete({
     const lastClickTargetRef = useRef();
     const renderedDataRef = useRef();
     const previousInputVal = useRef();
+    const optionalDataRef = useRef([]);
+
+    const [selectedIds, setSelectedIds] = useState(() => initialData.filter((d) => d.defaultSelected).map((d) => d.id));
+    optionalDataRef.current = optionalDataRef.current.filter((d) => selectedIds.find((id) => d.id == id));
+
+    const data = [...initialData, ...(allowNotPresent ? optionalDataRef.current : [])];
 
     useEffect(() => {
         renderedDataRef.current = JSON.stringify(data);
-
         const determLastClickTarget = (e) => (lastClickTargetRef.current = e.target);
         document.addEventListener("mousedown", determLastClickTarget);
         return () => document.removeEventListener("mousedown", determLastClickTarget);
@@ -38,12 +44,11 @@ export default function WaAutocomplete({
 
     const [suggestions, setSuggestions] = useState(data);
     const [areSuggsShown, setAreSuggsShown] = useState(false);
-    const [selectedIds, setSelectedIds] = useState(data.filter((d) => d.defaultSelected).map((d) => d.id));
-    const [inputVal, setInputValState] = useState(
-        multiple ? "" : props.data.find((d) => d.defaultSelected)?.name ?? "",
+    const [inputVal, setInputValState] = useState(() =>
+        multiple ? "" : data.find((d) => d.defaultSelected)?.name ?? "",
     );
-    const setInputVal = (newVal) => {
-        previousInputVal.current = inputVal;
+    const setInputVal = (newVal, prevVal) => {
+        previousInputVal.current = prevVal === undefined ? inputVal : prevVal;
         setInputValState(newVal);
     };
     const [invalid, setInvalid] = useState(false);
@@ -58,33 +63,73 @@ export default function WaAutocomplete({
 
     const suggsVisibility = (show) => {
         if (!show && inputVal) {
-            const inputItem = data.find((d) => d.name == inputVal);
+            let newData = data;
+            let inputItem = newData.find((d) => d.name == inputVal);
+            if (!multiple && selectedIds.length > 0 && selectedIds[0] == inputItem.id) inputItem = null;
             setAreSuggsShown(false);
-            setSelectedIds(inputItem ? [...selectedIds, inputItem.id] : selectedIds);
-            setInputVal(multiple ? (inputItem ? "" : inputVal) : inputVal);
-            setInvalid(!inputItem);
+            if (!inputItem && allowNotPresent && multiple) {
+                if (optionalDataRef.current.find((d) => d.name == inputVal)) inputItem = null;
+                else {
+                    inputItem = { id: generateId(), name: inputVal };
+                    optionalDataRef.current.push(inputItem);
+                    newData = [...initialData, ...optionalDataRef.current];
+                }
+                setInputVal("");
+            }
+            const newInputVal = multiple ? (inputItem ? "" : inputVal) : inputVal;
+            if (newInputVal) setInputVal(newInputVal);
+            setInvalid(inputItem === undefined && !allowNotPresent);
+            if (inputItem) {
+                const newSelectedIds = [...selectedIds, inputItem.id];
+                setSelectedIds(newSelectedIds);
+                if (onSelected)
+                    onSelected(
+                        multiple
+                            ? newData.filter((d) => newSelectedIds.includes(d.id))
+                            : newData.find((d) => d.id == inputItem.id),
+                    );
+            }
         } else if (areSuggsShown != show) {
             setAreSuggsShown(show);
             setInvalid(false);
         }
     };
     const onChangeNative = (e) => {
-        const val = typeof e == "string" ? e : e?.target?.value;
-        const newSelectedIds = multiple ? selectedIds : data.filter((d) => val && d.name == val).map((d) => d.id);
+        let val = typeof e == "string" ? e : e?.target?.value;
+        val = val ?? "";
+        let newSelectedId = multiple ? undefined : data.filter((d) => val && d.name == val);
+        if (newSelectedId)
+            if (newSelectedId.length > 0) newSelectedId = newSelectedId[0].id;
+            else newSelectedId = null;
+        if (
+            newSelectedId
+                ? selectedIds.length > 0 && selectedIds[0] == newSelectedId
+                : newSelectedId === null
+                  ? (!allowNotPresent ? true : !val && !previousInputVal.current) && selectedIds.length == 0
+                  : undefined
+        )
+            newSelectedId = undefined;
         setAreSuggsShown(true);
-        setInputVal(val ?? "");
+        setInputVal(val, !val && allowNotPresent ? "" : undefined);
         setSuggestions(
             !val || (!multiple && data.find((d) => d.name == val))
                 ? data
                 : data.filter((d) => d.name.startsWith(e.target.value)),
         );
-        setSelectedIds(newSelectedIds);
-        if (onChange) onChange(val);
-        if (!multiple && onSelected) {
-            const addedId = newSelectedIds.some((id) => !selectedIds.includes(id));
-            onSelected(addedId ? data.find((d) => d.id == addedId) : null);
+        if (newSelectedId) setSelectedIds([newSelectedId]);
+        if (newSelectedId === null) setSelectedIds([]);
+        if (onChange && val != inputVal) onChange(val);
+        if (!multiple && onSelected && newSelectedId !== undefined) {
+            const selectedItem = newSelectedId
+                ? data.find((d) => d.id == newSelectedId)
+                : val && allowNotPresent
+                  ? { id: "newItem", name: val }
+                  : null;
+            onSelected(selectedItem);
         }
     };
+    const refocusInput = (focus = true) =>
+        setTimeout(() => rootRef.current?.querySelector("input")[focus ? "focus" : "blur"]());
     const onSelectedNative = (id) => {
         if (id == "none") return;
         const selectedItem = data.find((d) => d.id == id);
@@ -97,9 +142,13 @@ export default function WaAutocomplete({
             : [id];
         setSelectedIds(newSelectedIds);
         setInputVal(multiple ? "" : selectedItem.name);
-        setSuggestions(data);
+        let newSuggestions = data;
+        const optionalItem = optionalDataRef.current.find((d) => id == d.id);
+        if (allowNotPresent && optionalItem && isDeselect)
+            newSuggestions = [...initialData, ...optionalDataRef.current.filter((d) => id != d.id)];
+        setSuggestions(newSuggestions);
         setAreSuggsShown(saveFocus);
-        setTimeout(() => rootRef.current?.querySelector("input")[saveFocus ? "focus" : "blur"]());
+        refocusInput(saveFocus);
         if (onSelected)
             onSelected(multiple ? data.filter((d) => newSelectedIds.includes(d.id)) : data.find((d) => d.id == id));
     };
@@ -205,10 +254,23 @@ export default function WaAutocomplete({
                     onBlur={(e) => {
                         if (!lastClickTargetRef.current?.closest("#" + id)) suggsVisibility(false);
                     }}
+                    onKeyDown={(e) => {
+                        if (e.code == "Escape") e.preventDefault();
+                    }}
                     onKeyUp={(e) => {
-                        if (e.code == "Escape" || e.code == "Enter") e.target.blur();
-                        if (e.code == "Backspace" && !previousInputVal.current)
-                            setSelectedIds(selectedIds.slice(0, -1));
+                        if (e.code == "Escape" || e.code == "Enter") {
+                            lastClickTargetRef.current = null;
+                            e.target.blur();
+                            if (e.code == "Enter" && allowNotPresent && multiple) refocusInput();
+                        }
+                        if (e.code == "Backspace" && !previousInputVal.current) {
+                            const lastId = selectedIds.at(-1);
+                            const newSelectedIds = selectedIds.slice(0, -1);
+                            setSelectedIds(newSelectedIds);
+                            const newOptionalData = optionalDataRef.current.filter((d) => d.id != lastId);
+                            if (allowNotPresent && optionalDataRef.current.length != newOptionalData.length)
+                                setSuggestions([...initialData, ...newOptionalData]);
+                        }
                         if (!inputVal && previousInputVal.current) previousInputVal.current = inputVal;
                     }}
                 />
